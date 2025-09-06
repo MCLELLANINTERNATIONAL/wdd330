13782398cee54dd836580703d2ad09be189131bb
bbagnes <bbagnes@byupathway.edu>
2025-09-05 21:13:10 -0600

    Changed formatting to remove a selected item from the cart.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,12 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setClick } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  // foreach (item in htmlItems) {
+  //   setClick(item, removeItemFromCart);
+  // }
 }
 
 function cartItemTemplate(item) {
@@ -19,10 +22,17 @@ function cartItemTemplate(item) {
   </a>
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
   <p class='cart-card__quantity'>qty: 1</p>
+  <span id='${item.Id}' class='close_button'>X</span>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
 </li>`;
+
+// setClick(this, removeItemFromCart);
 
   return newItem;
 }
 
 renderCartContents();
+
+function removeItemFromCart() {
+
+}
