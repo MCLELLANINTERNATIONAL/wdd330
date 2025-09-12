dccc50281e26b50558d008266fee543313c57bd6
bbagnes <bbagnes@byupathway.edu>
2025-09-12 13:46:46 -0600

    Added content per activity instructions.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import { updateCartBadge } from './product.js';
 import { getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import { ProductList } from './ProductList.mjs';
 
+const productData = new ProductData();
+const productList = new productList();
 const cart = getLocalStorage('so-cart') || [];
 updateCartBadge(cart.length);
