bb9b54854a6fe2f87f3aa66f721063ed3eb9208c
bbagnes <bbagnes@byupathway.edu>
2025-09-12 12:55:51 -0600

    Added advent listener for cart badge into init file.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -14,16 +14,26 @@ export default class ProductDetails {
         document
             .getElementById('addToCart')
             .addEventListener('click', this.addProductToCart.bind(this));
+            // adding listener to when the page is reloaded, to display the correct cart badge information.
+            addEventListener('DOMContentLoaded', () => {
+            const cartList = getLocalStorage('so-cart') || [];
+            console.log('Main page cartList:', cartList);
+            updateCartBadge(cartList.length);
+            });
     }
+    
     addProductToCart(product) {
         const cart = getLocalStorage('so-cart'); // always an array per your utils
         cart.push(this.product);
         setLocalStorage('so-cart', cart);
+        updateCartBadge();
     }
+    
     renderProductDetails() {
         productDetailsTemplate(this.product);
     }
 }
+
 function productDetailsTemplate(product) {
     document.querySelector('h2').textContent = product.Brand.Name;
     document.querySelector('h3').textContent = product.NameWithoutBrand;
