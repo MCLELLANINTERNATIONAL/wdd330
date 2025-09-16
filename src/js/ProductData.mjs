5e7633d0278b690eb9c5cf93370f372e1631ece8
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-09-16 22:27:47 +0100

    get category update

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -16,7 +16,6 @@ export default class ProductData {
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
-    
     return data.Result
   }
 
